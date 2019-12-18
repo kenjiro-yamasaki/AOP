57d68dc61e67ecfcd99338c5ab2d843ccf365a46
山﨑 謙次郎 <kenjiro.yamasaki@gmail.com>
2019-12-19 06:15:28 +0900

    [add]StringAppenderTestsを追加

--- src/Loggers.UnitTests/StringAppenderTests.cs
+++ src/Loggers.UnitTests/StringAppenderTests.cs
@@ -1,12 +1,32 @@
-﻿namespace SoftCube.Loggers.UnitTests
+﻿using Xunit;
+
+namespace SoftCube.Loggers.UnitTests
 {
     public class StringAppenderTests
     {
-
-
-        public class Logger
+        [Fact]
+        public void ToString_A_正しく変換する()
         {
+            var appender = new StringAppender();
+            appender.ConversionPattern = "{message}";
+            appender.Trace("A");
+
+            var actual = appender.ToString();
+
+            Assert.Equal("A", actual);
         }
 
+        [Fact]
+        public void ToString_AB_正しく変換する()
+        {
+            var appender = new StringAppender();
+            appender.ConversionPattern = "{message}";
+            appender.Trace("A");
+            appender.Trace("B");
+
+            var actual = appender.ToString();
+
+            Assert.Equal("AB", actual);
+        }
     }
 }
